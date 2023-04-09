6ba7363f3c73ffc79546605d9a9504f2b7850d3d
PabloPS0 <pablopierry.github.98@gmail.com>
2023-04-09 19:11:06 -0300

    Simple program to study data structures

---
+++ main.c
@@ -0,0 +1,13 @@
+// Array unidimensional
+int main() {
+    int idade;
+    char sexo;
+    float dinheiro;
+
+// Vetores
+    int idade[20]; 
+    int idade[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int idade[5] = {30,22,18,19};
+// [] = Tamanho do vetor
+// {} = Atribuição de valores ao vetor
+}
