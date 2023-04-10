b157043d082ddd853a0d9ba0fa7d7f715f007cc0
PabloPS0 <pablopierry.github.98@gmail.com>
2023-04-10 14:37:45 -0300

    Creating demonstrative variables

--- 02-criando_vetores.c
+++ 02-criando_vetores.c
@@ -1,3 +1,10 @@
+// Como criar variáveis e vetores:
+// O que é um vetor? Um conjunto de variáveis do mesmo tipo de dados armazenados em um bloco contíguo na memória do computador e são acessados ​​por meio de um índice.
+// Tipos de variáveis: Int = Inteiro | Char = Caractere | Float = Real
+// Identificador ou ID da variável = Nome da variável, Ex.: Idade
 int main() {
-    
+    int idade;
+    char sexo;
+    float nota;
+    return 0;
 }
